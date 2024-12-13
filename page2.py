ee524799341ec3163c3403e6f6d2c4bee602a986
mantin001 <116269093+mantin001@users.noreply.github.com>
2024-12-13 17:50:55 +0100

    Create page2.py

---
+++ page2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("HASIERAKO BIGARREN ORRIALDEA")
+st.write("2.EN ORRIA")
+st.page_link("streamlit_app.py", label="Etxea", icon="🏠")
+st.page_link("pages/pag1.py", label="1.go Orrialdea", icon="1️⃣")
